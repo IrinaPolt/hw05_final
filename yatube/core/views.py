628b3ddf33375015da479af29f4f9c8538757f8b
Irina Poltarykhina <irina.poltarykhina@gmail.com>
2022-05-01 22:21:38 +0300

    pep8

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -6,7 +6,9 @@ def page_not_found(request, exception):
 
 
 def csrf_failure(request, reason=''):
-    return render(request, 'core/403csrf.html', {'path': request.path}, status=403)
+    return render(request, 'core/403csrf.html',
+                  {'path': request.path}, status=403)
+
 
 def internal_server_error(request, reason=''):
     return render(request, 'core/500.html', {'path': request.path}, status=500)
